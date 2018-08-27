e5619b3d6de4a68ef19915a4544d4606f55255d8
Hsdiana <2831103860@qq.com>
2018-08-27 15:18:03 +0800

    修改环境配置
     - 添加代理
     - 添加打包输出文件路径
     - 修改备注格式
     - 修改测试环境服务器地址备注
     - 配置生产和测试环境的打包指令
     - 修改打包路径到package中进行区分
     - 配置测试环境运行时的命令
    
    Change-Id: Icb2deaedb1f22459b3ef6d568adcae8bb58d80ab

--- .webpackrc.js
+++ .webpackrc.js
@@ -4,10 +4,17 @@ export default {
   entry: 'src/index.js',
   extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
   env: {
+    // 开发环境
     development: {
       extraBabelPlugins: ['dva-hmr'],
+      publicPath: '/',
     },
+    // 打包时各个文件指向的路径
+    production: {
+      publicPath: process.env.SPACE_API_URL,
+    }
   },
+  outputPath: `./home/`,
   externals: {
     '@antv/data-set': 'DataSet',
     rollbar: 'rollbar',
@@ -23,7 +30,15 @@ export default {
   lessLoaderOptions: {
     javascriptEnabled: true,
   },
-  disableDynamicImport: true,
-  publicPath: '/',
+  proxy: {
+    '/space': {
+      target: process.env.SPACE_PROXY_URL, // 本地运行时启用代理访问的路由
+      changeOrigin: true,
+      pathRewrite: { '^/space': '' },
+    },
+  },
+  // true：关闭懒加载
+  // false：懒加载每一个页面（打包时每个页面打包为一个 js 文件，进入该页面才会加载此 js 文件）
+  disableDynamicImport: false,
   hash: true,
 };

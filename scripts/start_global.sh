2e3540c8f02c1437ea75c24504acac15d207a931
guomeng <835126160@qq.com>
2018-12-06 11:01:39 +0800

    添加海外部署配置文件
    Bug: SPAC-424
    Change-Id: I77aa7b0d6905eae4fd6d8fb2f84779f3893e219d

---
+++ scripts/start_global.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Copy all generated files into /root/qiniu
+rm -rf /root/qiniu
+mkdir -p /root/qiniu/
+cp -rf /root/dist /root/qiniu/global
+# Copy all static files into /root/dist/static
+rm -rf /root/dist/static
+mkdir -p /root/dist/static
+cp -rf /root/src/static/* /root/dist/static/
+# Upload all files to qiniu
+chmod +x /root/scripts/qshell
+/root/scripts/qshell account h07mPP3LHfjO8BHJfCyIRsiichflVYIHtyNkXNoM 6keig4uqFJFLjs80aLAPfjb3rnaMaiPOgRNJ9uik
+/root/scripts/qshell listbucket 9am-space global fileList
+/root/scripts/qshell batchdelete -force 9am-space fileList
+/root/scripts/qshell qupload /root/scripts/upload.config
+# Start server
+cd /root/server
+npm i
+node /root/server/app.js
